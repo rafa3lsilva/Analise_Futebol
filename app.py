95dcc2677f1f1436578fee77852bba074ea44893
rafael <rafaelpjmp@gmail.com>
2025-08-09 16:00:57 -0300

    mudulando e crando sidebar

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
 import re
-import data as db
+import data
+import sidebar
 
 
 def drop_reset_index(df):
@@ -51,27 +52,12 @@ st.sidebar.markdown(f"""
 """, unsafe_allow_html=True)
 st.sidebar.markdown("<br>", unsafe_allow_html=True)
 
-df = db.processar_dados()
+df = data.processar_dados()
 if 'Data' in df.columns:
     home_team = df["Time Referência"].unique()[0] if not df.empty else 'Home'
     away_team = df["Time Referência"].unique()[1] if not df.empty else 'Away'
 
-    #st.sidebar.header("📊 Análise de Jogos de Futebol")
-    st.sidebar.write("### Confronto:")
-
-    # Layout vertical centralizado
-    st.sidebar.markdown(f"""
-        <div style="text-align: center; font-size: 16px;">
-            <div style="margin-bottom: 10px; background-color:#1f77b4; padding:8px; border-radius:6px; color:white;">
-                🏠 {home_team}
-            </div>
-            <div style="margin-bottom: 5px;">⚔️ vs</div>
-            <div style="background-color:#d62728; padding:8px; border-radius:6px; color:white;">
-                ✈️ {away_team}
-            </div>
-        </div>
-    """, unsafe_allow_html=True)
-    st.sidebar.markdown("<br>", unsafe_allow_html=True)
+    sidebar.sidebar(home_team, away_team)
 
     # Seleção do intervalo de jogos com visual aprimorado
     st.markdown(
@@ -227,29 +213,7 @@ if 'Data' in df.columns:
         else:
             return "🔴 Baixa"
 
-    # Exibe os indicadores na sidebar
-
-    st.sidebar.markdown("### 🎯 Indicadores da Partida")
-
-    # Card BTTS
-    st.sidebar.markdown(f"""
-        <div style="background-color:#262730; padding:10px; border-radius:8px; text-align:center;">
-            <span style="font-size:16px; font-weight:bold; color:white;">🔍 BTTS (Ambas Marcam)</span><br>
-            <span style="font-size:20px; font-weight:bold; color:white;">{btts_home:.2f}</span><br>
-            <span style="font-size:16px;">{btts_status(btts_home, btts_away)}</span>
-        </div>
-    """, unsafe_allow_html=True)
-
-    st.sidebar.markdown("")
-
-    # Card Over 2.5
-    st.sidebar.markdown(f"""
-        <div style="background-color:#262730; padding:10px; border-radius:8px; text-align:center;">
-            <span style="font-size:16px; font-weight:bold; color:white;">🔍 Over 2.5 Gols</span><br>
-            <span style="font-size:20px; font-weight:bold; color:white;">{over_home:.2f}</span><br>
-            <span style="font-size:16px;">{over_status(over_home, over_away)}</span>
-        </div>
-    """, unsafe_allow_html=True)
+    sidebar.btts_over(btts_status, over_status, btts_home, btts_away, over_home, over_away)
 
     # Exibe os últimos jogos (Home)
     st.subheader(f"Últimos {num_jogos} jogos do {home_team}")
